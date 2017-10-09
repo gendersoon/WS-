ebc7c6ba5faaab38fbf87bf5b1f5b14d2ddc6db8
icarreons <akatsuki96_ivan@hotmail.com>
2017-10-09 13:59:01 -0500

    practica en equipo

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# WS-
+# STACK SCRAPER
+Web scraping in Python for StackOverflow.
+
+
+## Motivation
+Team school project for the ["Software Design and Architecture" class](https://github.com/AnhellO/DAS_Sistemas).
+
+
+## Libraries used
+- [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/)
+- [Requests](http://docs.python-requests.org/en/master/)
+- [Feedparser](https://pythonhosted.org/feedparser/index.html/)
+- [SQLAlchemy](https://www.sqlalchemy.org/)
+
+
+## Team
+
+|Team Member  |GitHub   |
+|:-:|:-:|
+|Omar Abasta |[ElFalsoAbraxas](https://github.com/ElFalsoAbraxas) |  
+|Iván Carreón |[gendersoon](https://github.com/gendersoon) |
+|Enrique Castillo |[ecete](https://github.com/ecete/)   |
+|Clemente Zúñiga |[fanborges](https://github.com/luis2297)   |
+|thx to Jaime Villarreal for his invaluable help |[jvillarreal7](https://github.com/jvillarreal7)   | 
+
--- práctica en equipo/Usuario.py
+++ práctica en equipo/Usuario.py
@@ -1,51 +1,55 @@
 from bs4 import BeautifulSoup
 import requests
-import time
                 
 class Usuario:
     
     #~ campos
-    
     __username = ''
     __bio = ''
     __soup = ''
     __url = ''
     __lista_comunidades = []
+    __userid = ''
     
     #~ constructor
-    def __init__(self,url,username):
+    def __init__(self,url,username,userid):
         self.__username = username
-        self.__soup = BeautifulSoup(requests.get(url).text,'html5lib')
+        self.__soup = BeautifulSoup(requests.get(url).text,'lxml')
         self.__url = url
+        self.__userid = userid
 
     #~ Métodos
+    
+    def get_userid(self):
+        return self.__userid
+        
     def get_username(self):
         return self.__username
     
     def get_bio(self):
         p = self.__soup.find(attrs = {'class': 'about'})
-        self.__bio = p.contents[5].text        
+        self.__bio = p.contents[5].text
         return self.__bio
     
     def get_comunidades(self):
         r = requests.get(self.__url)
         texto = r.text
-        soup_comunidades = BeautifulSoup(r.text, 'html5lib')
-        view_more = soup_comunidades.find(attrs = {'class':'view-more'})
+        x = BeautifulSoup(r.text, 'lxml')
+        view_more = x.find(attrs = {'class':'view-more'})
         if view_more == None:
-            comunidades = soup_comunidades.find_all(attrs = {'class':'community-name'})
+            comunidades = x.find_all(attrs = {'class':'community-name'})
             for i in comunidades:
                 self.__lista_comunidades.append(i.text.strip())        
         elif view_more.text.find("View network profile") != -1:
-            #print(view_more.a['href'])
-            #r = requests.get(view_more.a['href'])
-            #texto = r.text
-            #soup_comunidades = BeautifulSoup(r.text,'html5lib')
-            comunidades = soup_comunidades.find_all(attrs = {'class':'account-site'})
+            print(view_more.a['href'])
+            r = requests.get(view_more.a['href'])
+            texto = r.text
+            x = BeautifulSoup(r.text,'lxml')
+            comunidades = x.find_all(attrs = {'class':'account-site'})
             for i in comunidades:
                 self.__lista_comunidades.append(i.a.text.strip())   
         else:
-            comunidades = soup_comunidades.find_all(attrs = {'class':'community-name'})
+            comunidades = x.find_all(attrs = {'class':'community-name'})
             for i in comunidades:
                 self.__lista_comunidades.append(i.text.strip())    
         return self.__lista_comunidades
